495ef55e44fc77aece6f65f8d93e4b67c3905767
叶涛 <821023977@qq.com>
2019-10-18 11:04:55 +0800

    1

---
+++ install.bat
@@ -0,0 +1,26 @@
+﻿::公共基础模块
+ng install Senparc.Weixin &
+
+::微信小程序
+ng install Senparc.Weixin.WxOpen &
+
+::微信支付
+ng install Senparc.Weixin.TenPay &
+
+::ASP.NET MVC 扩展
+ng install Senparc.Weixin.MP.MVC &
+
+::微信企业号
+::ng install Senparc.Weixin.QY &
+
+::企业微信
+ng install Senparc.Weixin.Work &
+
+::微信开放平台
+ng install Senparc.Weixin.Open &
+
+::WebSocket 模块
+ng install Senparc.WebSocket &
+
+::Redis模块
+::ng install Senparc.Weixin.Cache.Redis
